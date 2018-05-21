bece88caf6fca73bb82ef8a96bc41719fc5ad489
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-05-21 01:12:18 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    raise params[:name].inspect
     if params[:name] == ""
       session[:name] = params[:name]
       redirect_to '/'
