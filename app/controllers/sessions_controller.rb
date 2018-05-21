747f0062f25d329fa21b629f8108a432c2eda742
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-05-21 01:10:51 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if session[:name] = params[:name]
-    redirect_to '/'
-  else
-    redirect_to '/login'
-  end
+    if params[:name]
+      session[:name] = params[:name]
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
   end
 
   def destroy
